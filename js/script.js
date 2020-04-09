91e1669be190f223e579b388cd9a4265210c5d1f
CharlotteLina <charlotte.gauthier@gonexa.fr>
2020-04-09 11:14:49 +0200

    Debut des filtres

--- js/script.js
+++ js/script.js
@@ -131,6 +131,7 @@ function getMoisDate(number)
 //Fonction pour le chargement de la page accueil
 function chargementPageIndex()
 {
+	slidderFunction();
 	var template = document.querySelector("#templateVoyage");
 	for (var v of lesDestinations) {					
 		let clone = document.importNode(template.content, true);   
@@ -143,6 +144,52 @@ function chargementPageIndex()
 		document.getElementById('containeVoyage').appendChild(clone);
 		temperature(v.id,v.destination);
 	};
+}
+
+function slidderFunction(){
+	$( function () {
+$( "#slider-range" ).slider({
+  range: true,
+  min: 0,
+  max: 2000,
+  step:20,
+  values: [ 50, 2000 ],
+  slide: function( event, ui ) {
+	$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+	//changementDestinationAfficheViaPrix();      
+  }
+});
+$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+  " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  console.log("lesDestination") 
+} );
+}
+function getVals(){
+// Get slider values
+var parent = this.parentNode;
+var slides = parent.getElementsByTagName("input");
+  var slide1 = parseFloat( slides[0].value );
+  var slide2 = parseFloat( slides[1].value );
+// Neither slider will clip the other, so make sure we determine which is larger
+if( slide1 > slide2 ){ var tmp = slide2; slide2 = slide1; slide1 = tmp; }
+
+var displayElement = parent.getElementsByClassName("rangeValues")[0];
+	displayElement.innerHTML = "$ " + slide1 + "k - $" + slide2 + "k";
+}
+
+window.onload = function(){
+// Initialize Sliders
+var sliderSections = document.getElementsByClassName("range-slider");
+	for( var x = 0; x < sliderSections.length; x++ ){
+	  var sliders = sliderSections[x].getElementsByTagName("input");
+	  for( var y = 0; y < sliders.length; y++ ){
+		if( sliders[y].type ==="range" ){
+		  sliders[y].oninput = getVals;
+		  // Manually trigger event first time to display values
+		  sliders[y].oninput();
+		}
+	  }
+	}
 }
 //Récupération de la température et maj du site
 function temperature(id,dest){
